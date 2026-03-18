e743a3fc509d84b268e2d0de0b75da5ba21172c2
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-03-18 19:57:42 +0530

    Added insertion sorting file

---
+++ Arrays/InsertionSort.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class InsertionSort {
+    public static void main(String args[]){
+        int arr[]={7,8,3,1,2};
+        //insertion sort
+        for(int i=0;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        for(int k=0;k<arr.length;k++){
+            System.out.print(arr[k]);
+        }
+    }
+    
+}
